04ba7c5fcc2d67b052bc666c02c0a3098cd0173d
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2025-10-14 15:28:35 +0300

    Added example of SFML rendering with EnTT

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <entt/entt.hpp>
 
 #include <SFML/Graphics.hpp>
@@ -7,33 +8,47 @@ struct Position {
     float x, y;
 };
 
-entt::handle create_handle(entt::registry &registry) {
+entt::handle CreateHandle(entt::registry &registry) {
     return {registry, registry.create()};
+}
+
+[[maybe_unused]] bool SetActive(sf::RenderWindow *const window, bool active) {
+    return window->setActive(active);
+}
+
+void RenderThread(sf::RenderWindow *const window, const entt::registry *const registry) {
+    SetActive(window, true);
+
+    while (window->isOpen()) {
+        window->clear(sf::Color::Black);
+
+        for (const auto &[entity, position]: registry->view<Position>().each()) {
+            sf::RectangleShape rectangle{{50.f, 50.f}};
+            rectangle.setPosition({position.x, position.y});
+
+            window->draw(rectangle);
+        }
+
+        window->display();
+    }
 }
 
 int main() {
     entt::registry registry;
-    const auto entity = create_handle(registry);
+    const auto entity = CreateHandle(registry);
     entity.emplace<Position>(10.f, 20.f);
 
     sf::RenderWindow window(sf::VideoMode{{800u, 600u}}, "Window");
+    SetActive(&window, false);
+
+    std::jthread renderThread(&RenderThread, &window, &registry);
 
     while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
+        while (const auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
-        window.clear();
-
-        for (const auto &[entity, position]: registry.view<Position>().each()) {
-            sf::RectangleShape rectangle{{50.f, 50.f}};
-            rectangle.setPosition({position.x, position.y});
-            window.draw(rectangle);
-        }
-
-        window.display();
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
