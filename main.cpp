1b8e424bdb9c4474d929058e0eedca6d01a42dc4
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2025-10-14 15:36:21 +0300

    Added .clang-format

--- main.cpp
+++ main.cpp
@@ -1,54 +1,64 @@
 #include <iostream>
 #include <thread>
+
 #include <entt/entt.hpp>
 
 #include <SFML/Graphics.hpp>
 
-struct Position {
-    float x, y;
+struct Position
+{
+	float x, y;
 };
 
-entt::handle CreateHandle(entt::registry &registry) {
-    return {registry, registry.create()};
+entt::handle CreateHandle(entt::registry& registry)
+{
+	return { registry, registry.create() };
 }
 
-[[maybe_unused]] bool SetActive(sf::RenderWindow *const window, bool active) {
-    return window->setActive(active);
+[[maybe_unused]] bool SetActive(sf::RenderWindow* const window, bool active)
+{
+	return window->setActive(active);
 }
 
-void RenderThread(sf::RenderWindow *const window, const entt::registry *const registry) {
-    SetActive(window, true);
+void RenderThread(sf::RenderWindow* const window, const entt::registry* const registry)
+{
+	SetActive(window, true);
 
-    while (window->isOpen()) {
-        window->clear(sf::Color::Black);
+	while (window->isOpen())
+	{
+		window->clear(sf::Color::Black);
 
-        for (const auto &[entity, position]: registry->view<Position>().each()) {
-            sf::RectangleShape rectangle{{50.f, 50.f}};
-            rectangle.setPosition({position.x, position.y});
+		for (const auto& [entity, position] : registry->view<Position>().each())
+		{
+			sf::RectangleShape rectangle{ { 50.f, 50.f } };
+			rectangle.setPosition({ position.x, position.y });
 
-            window->draw(rectangle);
-        }
+			window->draw(rectangle);
+		}
 
-        window->display();
-    }
+		window->display();
+	}
 }
 
-int main() {
-    entt::registry registry;
-    const auto entity = CreateHandle(registry);
-    entity.emplace<Position>(10.f, 20.f);
+int main()
+{
+	entt::registry registry;
+	const auto entity = CreateHandle(registry);
+	entity.emplace<Position>(10.f, 20.f);
 
-    sf::RenderWindow window(sf::VideoMode{{800u, 600u}}, "Window");
-    SetActive(&window, false);
+	sf::RenderWindow window(sf::VideoMode{ { 800u, 600u } }, "Window");
+	SetActive(&window, false);
 
-    std::jthread renderThread(&RenderThread, &window, &registry);
+	std::jthread renderThread(&RenderThread, &window, &registry);
 
-    while (window.isOpen()) {
-        while (const auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-    }
+	while (window.isOpen())
+	{
+		while (const auto event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
